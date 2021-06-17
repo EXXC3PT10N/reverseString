eb6de15ec60510806f949139728da00eedc8921f
Mikołaj Szczubiał <32496927+EXXC3PT10N@users.noreply.github.com>
2021-06-17 20:15:11 +0200

    Final

---
+++ reverseString.js
@@ -0,0 +1,53 @@
+const reverse = (input) => {
+  const words = [];
+  const otherChars = [];
+
+  let output = "";
+  let wordCount = 0;
+  let isLastCharLetter = true;
+
+  for (let i = 0; i < s.length; i++) {
+    let c = s[i];
+  
+    if ((c.charCodeAt(0) > 64 && c.charCodeAt(0) < 91) || (c.charCodeAt(0) > 96 && c.charCodeAt(0) < 123)) {
+      if (words[wordCount] != null)
+        words[wordCount].push(c);
+      else {
+        words[wordCount] = [];
+        words[wordCount].push(c);
+      }
+      isLastCharLetter = true;
+    } else {
+      if (isLastCharLetter) {
+        otherChars.push([c]);
+        wordCount++;
+  
+      } else {
+        otherChars[otherChars.length - 1].push(c);
+      }
+      isLastCharLetter = false;
+    }
+  
+  }
+  
+  
+  words.reverse();
+  for (let i = 0; i < words.length; i++) {
+    for (let j = 0; j < words[i].length; j++) {
+      output += words[i][j];
+    }
+    if (otherChars.length > i) {
+      for (let j = 0; j < otherChars[i].length; j++) {
+        output += otherChars[i][j];
+      }
+    }
+  }
+  // console.log(words);
+  // console.log(otherChars);
+  return output;
+}
+
+const s = "AAaa Zzz?    qwer   ;;;";
+console.log(`input: ${s}`);
+console.log(`output: ${reverse(s)}`);
+
